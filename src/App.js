ba2b00d110a3d877a6756b145ff29c1026f4c0f3
araisl <raisl@taktsoft.com>
2023-04-26 20:05:06 +0200

    Add navbar again

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import "./App.css";
 const App = () => {
   return (
     <>
-      {/* <NavBar /> */}
+      <NavBar />
       <Outlet />
       {/* <Footer /> */}
     </>
--- src/components/PostPreviewCard.js
+++ src/components/PostPreviewCard.js
@@ -1,3 +1,9 @@
+// Imports from React libraries
+import { Link } from "react-router-dom";
+// Imports for own Components
+import BaseButton from "./BaseButton";
+
+// imports from Material UI
 import { Paper } from "@mui/material"
 import PostPreview from "./postPreview"
 import Grid from '@mui/material/Grid';
@@ -5,6 +11,7 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import PersonIcon from '@mui/icons-material/Person';
 import Rating from '@mui/material/Rating';
+import CategoryIcon from '@mui/icons-material/Category';
 
 import sanityClient from "../cmsClient";
 import imageUrlBuilder from "@sanity/image-url";
@@ -24,12 +31,23 @@ const simpleBoxStyles = {
 	marginTop: 2
 };
 
+const buttonStyles = {
+	fontSize: '0.875rem',
+	fontWeight: 600,
+	backgroundColor: '#F09440',
+	textTransform: 'capitalize',
+	borderRadius: 2.5,
+	'&:hover': {
+		backgroundColor: '#F0B36A'
+	}
+};
+
 const PostPreviewCard = ({post}) => {
   return (
     <Grid item xs={4}>
       <Paper elevation={3}>
         <img className="img" src={urlFor(post.mainImage).width(200).url()} alt={post.title} />
-				<Box paddingX={1} marginBottom={3}>
+				<Box paddingX={1}>
         	<Typography sx={postTitleStyles} variant="h6" component="h2">
           	{ post.title }
         	</Typography>
@@ -40,12 +58,33 @@ const PostPreviewCard = ({post}) => {
 						</Typography>
 					</Box>
 					<Box sx={simpleBoxStyles}>
+						<CategoryIcon sx={{width: 20}} />
+						{
+							post.categories.map((cat) => {
+								return (
+									<Link to={"/filteredposts/" + cat.slug.current}>{ cat.title }</Link>
+								)
+							})
+						}
+					</Box>
+					<Box sx={simpleBoxStyles}>
 						<Typography>
 							Beschreibung: {post.description}
 						</Typography>
 					</Box>
 					<Box sx={simpleBoxStyles}>
 						<Rating name="read-only" value={4.5} precision={0.5} readOnly />
+						<Typography variant="body2" component="p" marginLeft={0.8}>
+							4.5
+						</Typography>
+						<Typography variant="body2" component="p" marginLeft={0.8}>
+							(933 Reviews)
+						</Typography>
+					</Box>
+					<Box sx={simpleBoxStyles} >
+						<Link to={"/post/" + post.slug.current} >
+							<BaseButton sx={buttonStyles} size="large" color="success" variant="contained">Lesen</BaseButton>
+						</Link>
 					</Box>
 
 				</Box>
